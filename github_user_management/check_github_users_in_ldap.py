edb3da90742f7b3ec0ddd816e9e1943630ed8a67
Dave Zolotusky <dzolo@spotify.com>
2021-11-15 21:11:28 +0100

    add options to remove non-matching and non-employee users
    optionally remove users from github org that aren't employees anymore
    also optionally remove users with github users names that don't match
    any users in the ldap org

--- github_user_management/check_github_users_in_ldap.py
+++ github_user_management/check_github_users_in_ldap.py
@@ -14,16 +14,16 @@ def print_dict_keys_per_value(d, exclude, check_existence_for_these):
     for v in sorted(by_value.keys()):
         if v in exclude:
             continue
-        print ("%s (%i)" % (v, len(by_value[v])))
+        print("%s (%i)" % (v, len(by_value[v])))
         printed_categories.append(v)
         for k in sorted(by_value[v]):
-            print (" " + k)
+            print(" " + k)
     for cat in check_existence_for_these:
         if cat not in printed_categories:
-            print ("Good news, category %s is empty!" % cat)
+            print("Good news, category %s is empty!" % cat)
 
 
-def check_github_usernames(github_token, ldap_url, ldap_base, github_url, org):
+def check_github_usernames(github_token, ldap_url, ldap_base, github_url, org, remove_nonemployee, remove_nonmatching):
     gc = github_client.GithubClient(github_token, github_url)
 
     members = dict(
@@ -36,12 +36,18 @@ def check_github_usernames(github_token, ldap_url, ldap_base, github_url, org):
             if shell.decode('UTF-8') == '/dev/null':
                 if github_user in members and members[github_user] == 'only_in_github':
                     members[github_user] = 'github_user_that_quit'
+                    if remove_nonemployee:
+                        gc.remove_member(org, github_user)
                 continue
             if github_user not in members:
                 members[github_user] = 'to_add_to_github'
             else:
                 members[github_user] = 'matching'
                 # print "matching %s@spotify.com" % user
+
+        if remove_nonmatching:
+            for non_matching_member in [m for m in members if members[m] == 'only_in_github']:
+                gc.remove_member(org, non_matching_member)
 
     print_dict_keys_per_value(
         members, ("matching", "to_add_to_github"),
